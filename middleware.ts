92fdebd9615e747a1cbc91b2c34e43e520deb636
pks2906 <pratik1971sinha@gmail.com>
2024-07-04 20:32:24 +0530

    change

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 const isPublicRoute= createRouteMatcher(['/sign-in', '/sign-up', '/'])
 
  export default clerkMiddleware((auth, req) => {
-  // Restrict admin route to users with specific role
+
   if (!isPublicRoute(req )) auth().protect();
 
 
