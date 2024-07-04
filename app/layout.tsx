653c7884982083fd2571162268397c02e1db82ab
pks2906 <pratik1971sinha@gmail.com>
2024-07-04 20:30:59 +0530

    replace client-to-clerk & middleware added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ConvexClientProvider } from "./providers/ConvexProvider";
+import ConvexClerkProvider  from "./providers/ConvexClerkProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,10 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-      <ConvexClientProvider>
+      <ConvexClerkProvider>
          {children}
-      </ConvexClientProvider>
+      </ConvexClerkProvider>
         </body>
     </html>
   );
-}
+} 
